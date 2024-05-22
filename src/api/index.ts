822183f8531f7d39eba2c0d6d785ef6ee52a137a
Nuttanan29445 <nuch29445@gmail.com>
2024-05-22 19:54:31 +0700

    'ConfigVercel'

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes/groupDataRoute");
+const routes = require("../routes/groupDataRoute");
 
 const app = express();
 const port = 3000;
